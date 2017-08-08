326ffb40b8c045a74b4d07f955c4598275be8bbc
郑新新 <zhengxinxin@zhengxinxindeMacBook-Pro.local>
2017-08-08 22:48:18 +0800

    all

--- WebApi/src/App.vue
+++ WebApi/src/App.vue
@@ -1,9 +1,20 @@
 <template>
   <div id="myApp">
-  	<header class="header">前端API</header>
-  	<div class="body">
-  		<div class="left-sidebar">left</div>
-  		<div class="right-content">right</div>
+  	<header class="header flex-row">
+  		<div class="flex3"></div>
+  		<ul class="header-nav flex7">
+  			<li><a>基础知识</a></li>
+  			<li><a>框架</a></li>
+  			<li><a>工具</a></li>
+  		</ul>
+  	</header>
+  	<div class="content flex-row">
+  		<div class="left-sidebar flex2">
+  			<ul>
+  			
+  			</ul>
+  		</div>
+  		<div class="right-content flex8">right</div>
   	</div>
   	<footer class="footer">底部</footer>
   </div>
